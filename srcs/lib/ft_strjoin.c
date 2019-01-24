300a9de6834a2304c29d4af9a3f5b3f31adfb158
Cedric M'PASSI <cempassi@e3r1p2.42.fr>
2019-01-24 12:32:24 +0100

    modif strjoin

--- srcs/lib/ft_strjoin.c
+++ srcs/lib/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 15:04:07 by cempassi          #+#    #+#             */
-/*   Updated: 2019/01/17 14:41:17 by cempassi         ###   ########.fr       */
+/*   Updated: 2019/01/24 11:05:43 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char		*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*join;
 
-	if (!(join = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + 1)))
+	if (!(join = ft_strnew(ft_strlen(s1) + ft_strlen(s2))))
 		return (NULL);
 	ft_strcpy(join, s1);
 	ft_strcat(join, s2);
