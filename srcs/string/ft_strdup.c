1f9eca5258534d6aed310f78c18ab735bf27e3a1
Cedric M'PASSI <cempassi@student.42.fr>
2019-02-21 19:24:31 +0100

    ft strdup correction

--- srcs/string/ft_strdup.c
+++ srcs/string/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/19 16:45:20 by cempassi          #+#    #+#             */
-/*   Updated: 2019/01/17 14:41:17 by cempassi         ###   ########.fr       */
+/*   Updated: 2019/02/20 01:27:15 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strdup(const char *s1)
 {
 	char	*dst;
 
+	if (!s1)
+		return (NULL);
 	if (!(dst = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1)))
 		return (NULL);
 	return (ft_strcpy(dst, s1));
