9b8fb328d9f6ae29179e2ef4cb410abd6954602b
Cedric M'PASSI <cempassi@student.42.fr>
2019-03-01 23:46:42 +0100

    fix lstcpy

--- srcs/list/ft_lstcpy.c
+++ srcs/list/ft_lstcpy.c
@@ -6,13 +6,13 @@
 /*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/01 22:30:05 by cempassi          #+#    #+#             */
-/*   Updated: 2019/03/01 22:32:51 by cempassi         ###   ########.fr       */
+/*   Updated: 2019/03/01 23:45:59 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list 			*ft_lstcpy(t_list *source)
+t_list 			*ft_lstcpy(t_list *source, void (*cpy)(void *, void *))
 {
 	t_list		*copy;
 	t_list		*node;
@@ -20,8 +20,10 @@ t_list 			*ft_lstcpy(t_list *source)
 	copy = NULL;
 	while (source)
 	{
-		node = ft_lstnew(source->data, source->data_size);	
-		ft_lstaddback(&copy, node);
+		if(!(node = ft_lstnew(source->data, source->data_size)))
+			return (NULL);
+		cpy(node->data, source->data);
+		ft_lstadd(&copy, node);
 		source = source->next;
 	}
 	return (copy);
