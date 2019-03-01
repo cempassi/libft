bee9f1fea1c97d38731e12f9d48e13c2dfb9e33d
Cedric M'PASSI <cempassi@student.42.fr>
2019-03-02 00:28:38 +0100

    update lstcpy

--- srcs/list/ft_lstcpy.c
+++ srcs/list/ft_lstcpy.c
@@ -6,13 +6,13 @@
 /*   By: cempassi <cempassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/01 22:30:05 by cempassi          #+#    #+#             */
-/*   Updated: 2019/03/01 23:45:59 by cempassi         ###   ########.fr       */
+/*   Updated: 2019/03/02 00:28:23 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list 			*ft_lstcpy(t_list *source, void (*cpy)(void *, void *))
+t_list 			*ft_lstcpy(t_list *source, int (*cpy)(void *, void *))
 {
 	t_list		*copy;
 	t_list		*node;
